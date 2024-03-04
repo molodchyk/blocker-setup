58a889ecd4cc7ab0335728d59477022898701f54
molodchyk <73010708+molodchyk@users.noreply.github.com>
2024-03-04 20:25:07 +0100

    adb.exe script to block Play Store on BOOX Note 2

---
+++ platform-tools/timelimit.bat
@@ -0,0 +1,5 @@
+adb.exe shell pm uninstall -k --user 0 com.android.vending
+adb.exe shell pm uninstall -k --user 0 com.google.android.gms
+adb.exe shell pm uninstall -k --user 0 com.android.email
+adb.exe shell pm uninstall -k --user 0 com.onyx.mail
+pause
